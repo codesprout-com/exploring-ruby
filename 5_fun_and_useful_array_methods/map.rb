3cbc26324efb9848dca9ff65d66ea7f6205eedb0
Cliff <cliff@codesprout.com>
2023-01-06 16:00:12 -0500

    Adds examples demonstrating fun and useful Ruby Array methods

---
+++ 5_fun_and_useful_array_methods/map.rb
@@ -0,0 +1,7 @@
+names = [ "Cliff", "Alice", "Bob", "Kyle", "Shannon" ]
+
+upcased_names = names.map do |name|
+  name.upcase
+end
+
+p upcased_names
---
+++ 5_fun_and_useful_array_methods/shuffle.rb
@@ -0,0 +1,5 @@
+cards = [ "AS", "QH", "10S", "9D", "7H", "2S" ]
+
+p cards.shuffle
+p cards.shuffle
+p cards.shuffle
